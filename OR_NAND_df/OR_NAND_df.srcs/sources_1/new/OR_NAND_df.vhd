2ecf263bedcaca2b02eeca5594932aabb369465a
Tamojit Das <tamojitdas181007@gmail.com>
2025-05-04 20:41:55 +0530

    OR using NAND

---
+++ OR_NAND_df/OR_NAND_df.srcs/sources_1/new/OR_NAND_df.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04.05.2025 20:18:51
+-- Design Name: 
+-- Module Name: OR_NAND_df - dataflow
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity OR_NAND_df is
+    Port ( x : in STD_LOGIC;
+           y : in STD_LOGIC;
+           z : out STD_LOGIC);
+end OR_NAND_df;
+
+architecture dataflow of OR_NAND_df is
+
+begin
+z <= (x NAND x) NAND (y NAND y);
+end dataflow;
